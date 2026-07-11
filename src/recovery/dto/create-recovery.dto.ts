d4ccbd4e9743a8d15ae925eb83f44bafaf709f5d
favour-gl <favourabrahamko@gmail.com>
2026-07-11 23:34:24 +0100

    feat

--- src/recovery/dto/create-recovery.dto.ts
+++ src/recovery/dto/create-recovery.dto.ts
@@ -1,1 +1,26 @@
-export class CreateRecoveryDto {}
+import { IsString, IsNotEmpty, IsOptional, Matches } from 'class-validator';
+
+const STELLAR_PUBLIC_KEY_PATTERN = /^G[A-Z2-7]{55}$/;
+
+export class CreateRecoveryDto {
+  @IsString()
+  @Matches(STELLAR_PUBLIC_KEY_PATTERN, {
+    message: 'accountId must be a valid Stellar public key',
+  })
+  accountId: string;
+
+  @IsString()
+  @Matches(STELLAR_PUBLIC_KEY_PATTERN, {
+    message: 'newPublicKey must be a valid Stellar public key',
+  })
+  newPublicKey: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  proof?: string;
+
+  @IsOptional()
+  @IsString()
+  reason?: string;
+}
